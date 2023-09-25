1d6f455ed56da8bc3f578600d8c97805942c77e8
Yuno1234 <61713594+Yuno1234@users.noreply.github.com>
2023-09-25 17:17:24 +1300

    complete 3a

--- 3a-problem-solutions.rkt
+++ 3a-problem-solutions.rkt
@@ -188,3 +188,118 @@
 (define (handle-key c ke)
   (cond [(key=? ke " ") 10]
         [else c]))
+
+
+
+;;============================================================
+;; traffic-light
+;;============================================================
+;; Constants:
+ (define RADIUS 20)
+ (define SPACING 5)
+
+ (define SPACE (square SPACING "solid" "black"))
+ 
+ (define BACKGROUND (rectangle (+ (* SPACING 2) (* RADIUS 2))
+                               (+ (* SPACING 4) (* RADIUS 6))
+                               "solid"
+                               "black"))
+ (define R-ON (overlay (above SPACE
+                              (circle RADIUS "solid" "red")
+                              SPACE
+                              (circle RADIUS "outline" "yellow")
+                              SPACE
+                              (circle RADIUS "outline" "green")
+                              SPACE)
+                       BACKGROUND))
+
+ (define Y-ON (overlay (above SPACE
+                              (circle RADIUS "outline" "red")
+                              SPACE
+                              (circle RADIUS "solid" "yellow")
+                              SPACE
+                              (circle RADIUS "outline" "green")
+                              SPACE)
+                       BACKGROUND))
+
+ (define G-ON (overlay (above SPACE
+                              (circle RADIUS "outline" "red")
+                              SPACE
+                              (circle RADIUS "outline" "yellow")
+                              SPACE
+                              (circle RADIUS "solid" "green")
+                              SPACE)
+                       BACKGROUND))
+ 
+
+
+;; =================
+;; Data definitions:
+
+;; TraficLight is one of:
+;   - "red"
+;   - "yellow"
+;   - "green"
+; inter. colors of the trafic light
+; <examples are redundant for enumerations>
+
+(define (fn-for-trafic-light l)
+  (cond [(string=? l "red") (...)]
+        [(string=? l "yellow") (...)]
+        [(string=? l "green") (...)]))
+
+; Templates rules used:
+;  - one of: 3cases
+;  - atomic distinct: "red"
+;  - atomic distinct: "yellow"
+;  - atomic distinct: "green"
+
+;; =================
+;; Functions:
+
+;; TL -> TL
+;; start the world with (main "red")
+;; 
+(define (main l)
+  (big-bang l                          ; TL
+            (on-tick   next-color 1)   ; TL -> TL
+            (to-draw   render)))       ; TL -> Image
+
+
+;; TL -> TL
+;; produce the next trafic light
+(check-expect (next-color "red") "green")
+(check-expect (next-color "green") "yellow")
+(check-expect (next-color "yellow") "red")
+
+;(define (next-color l) "yellow") ;stub
+
+(define (next-color l)
+  (cond [(string=? l "red") "green"]
+        [(string=? l "yellow") "red"]
+        [(string=? l "green") "yellow"]))
+
+
+;; TL -> Image
+;; render the Image for the next trafic light
+(check-expect (render "red") R-ON)
+(check-expect (render "yellow") Y-ON)
+(check-expect (render "green") G-ON)
+    
+;(define (render l) Y-ON) ; stub
+
+(define (render l)
+  (cond [(string=? l "red") R-ON]
+        [(string=? l "yellow") Y-ON]
+        [(string=? l "green") G-ON]))
+
+
+
+
+
+
+
+
+
+
+
